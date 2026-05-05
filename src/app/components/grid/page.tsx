17ab7d4b2e01eca5efcfb597873e0e46ffab4798
Tim Dwyer <timdwyer@Tims-MacBook-Pro.local>
2026-05-05 17:03:56 -0500

    changed filepaths

--- src/app/components/grid/page.tsx
+++ src/app/components/grid/page.tsx
@@ -2,12 +2,13 @@ import styles from "./grid.module.css";
 import LoaderCard from "../card/card";
 
 const LOADERS = [
-  { id: 1, label: "Option 01", file: "./images/loader-1.lottie" },
-  { id: 2, label: "Option 02", file: "./images/loader-2.lottie" },
-  { id: 3, label: "Option 03", file: "./images/loader-3.lottie" },
-  { id: 4, label: "Option 04", file: "./images/loader-4.lottie" },
-  { id: 5, label: "Option 05", file: "./images/loader-5.lottie" },
-  { id: 6, label: "Option 06", file: "./images/loader-6.lottie" },
+  { id: 1, label: "Option 01", file: "loader-1.lottie" },
+  { id: 2, label: "Option 02", file: "loader-2.lottie" },
+  { id: 3, label: "Option 03", file: "loader-3.lottie" },
+  { id: 4, label: "Option 04", file: "loader-4.lottie" },
+  { id: 5, label: "Option 05", file: "loader-5.lottie" },
+  { id: 6, label: "Option 06", file: "loader-6.lottie" },
+
 ];
 
 export default function LoaderGrid() {
